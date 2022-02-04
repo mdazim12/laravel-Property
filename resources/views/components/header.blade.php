8b122f9fd75a1a26dfe5dddbb85a6f7a42b2ed2d
mdazim12 <azimuddin807@gmail.com>
2022-02-05 04:06:53 +0600

    header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,23 @@
+<div class="px-4 fixed w-full justify-between py-12  items-center flex">
+    <div class="min-max">
+        <img width="120px" src="/img/logo1.png" alt="image">
+     </div>
+
+   <div class="w-full ">
+      <ul class="flex justify-center" >
+          <li><a class="inline-block p-4 " href="#">Land</a></li>
+          <li><a class="inline-block p-4 " href="#">Villa</a></li>
+          <li><a class="inline-block p-4 " href="#">Apartment</a> </li>
+          <li><a class="inline-block p-4 " href="#">About Us</a></li>
+          <li><a class="inline-block p-4 " href="#">Contact</a></li>
+      </ul>
+   </div>
+
+
+
+     <div class="min-w-max text-lg flex">
+         <a class="p-2" href="">ENG </a>
+         <a class="p-2" href="">BD </a>
+     </div>
+
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<x-guest-layout>
+
+  <div class="realtive z-10 pt-10 pb-24 bg-cover bg-center m-0"      style="background-image:url('/img/hero1.png')">
+
+     <div class="absoulte h-full w-full bg-black opacity-30 top-0 left-0"></div>
+     <div class="container max-auto z-20">
+      <h2 class="mt-20  text-center text-red-400 " >Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />Atque ea consectetur dignissimos! Deserunt sapiente vel sit perspiciatis </h2>
+
+      <p class="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, incidunt.</p>
+
+      </div>
+
+  </div>
+</x-guest-layout>
